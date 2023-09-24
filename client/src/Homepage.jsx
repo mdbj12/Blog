3dbaef6e202bb6449d8518db131cb523230fbfbf
Michael Jeung <michaelj229@gmail.com>
2023-09-24 16:37:22 -0400

    retry

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 import Navbar from "./Navbar";
 import THREE_D_OBJECT from './THREE_D_OBJECT'
 
--- client/src/Three_D_Object.jsx
+++ client/src/Three_D_Object.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
